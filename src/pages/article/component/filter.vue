b9291629fee7e215e6cb099f8c6b75af9e46a861
王武侯 <3144216129@qq.com>
2024-04-03 11:06:00 +0800

    1

--- src/pages/article/component/filter.vue
+++ src/pages/article/component/filter.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>
-    filter
+  <div class="container">
+    <h1>filter过滤器</h1>
+    <h4>原数据{{ numbers }}</h4>
+    <h4>filter过滤的数据{{ numbers }}</h4>
+    
+
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const evenNumbers = numbers.filter(item => item % 2 == 0)
+
+
+
 
 </script>
 
 <style scoped>
+.container {
+  margin-left: 50px;
 
+}
 </style>
