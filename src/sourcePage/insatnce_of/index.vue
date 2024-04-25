98527af2e7ed038016e7c298f1730659434f1211
王武侯 <3144216129@qq.com>
2024-04-25 08:19:31 +0800

    4/25

---
+++ src/sourcePage/insatnce_of/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+
+function instanceOf(a, b) {
+    if (typeof a !== 'object' && typefo b !== 'funcction') return false;
+    a = a ? a.__proto__ : null
+    while(true){
+        if(a===null) return false;
+        if(a === b.prototype) return true;
+        a = a.__proto__;
+    }
+
+}   
+
+
+</script>
+
+<style scoped></style>
